1271357f020d1508e9cbbc7a5a6a0f0e16d35e8e
Jaylon Nelson-Sellers <jcns@uw.edu>
2021-12-10 14:04:23 -0800

    Update ThreadedBST.h

--- ThreadedBST.h
+++ ThreadedBST.h
@@ -7,25 +7,26 @@
 using namespace std;
 
 class BSTNode {
-	friend class ThreadedBST;	
+	friend class ThreadedBST;
 private:
 	explicit BSTNode(int item);
 	int item; //Value
-	BSTNode *leftChildPtr; //Left Pointer
-	BSTNode *rightChildPtr; //Right Pointer
+	BSTNode* leftChildPtr; //Left Pointer
+	BSTNode* rightChildPtr; //Right Pointer
 	bool isThreadedLeft;	//Bool of left Thread
 	bool isThreadedRight;	//Bool of Right Thread
 };
 
 class ThreadedBST {
-	friend ostream &operator<<(ostream &out, const ThreadedBST &tree);
+	//friend ostream& operator<<(ostream& out, const ThreadedBST& tree);
 
 private:
 	BSTNode* root = nullptr;
+	int count;
 
 public:
 	// Parameterized constructor
-	explicit ThreadedBST(int rootItem = 0);
+	explicit ThreadedBST();
 
 	//This is a different method from the one above, 
 	// This one creates n number of nodes
@@ -35,11 +36,20 @@ public:
 	//Destructor
 	virtual ~ThreadedBST();
 
-	//Get Depth
-	int getDepth() const;
+	void insertMultipleNodes(const int& min, const int& max);
+
+	BSTNode* insert(BSTNode* node, const int& data);
+
+
 
 	// Insert function
-	void Insert(ThreadedBST* root, int item);
+	//void Insert(const int& item);
+
+	void setThread(BSTNode* node);
+
+	BSTNode* getFarthestLeft(BSTNode* node) const;
+
+	BSTNode* getFarthestRight(BSTNode* node) const;
 
 	// Remove
 	void remove(BSTNode& node, int value);
@@ -50,10 +60,15 @@ public:
 	// Remove Even
 	void removeEven();
 
+	//void Inorder();
+
 	// Completes an inorder traversal of the threaded BST
-	void Inorder();
+	//void Inorder(BSTNode* node);
+
+	//Get Depth
+	int getDepth() const;
 
 	// Clear
-	void clear();
+	void clear(BSTNode* node);
 };
 #endif
