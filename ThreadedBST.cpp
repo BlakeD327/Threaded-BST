53191880364dabe8f12817caa6ec6a1116a50ad4
BlakeD327 <47874373+BlakeD327@users.noreply.github.com>
2021-12-10 14:56:14 -0800

    Update ThreadedBST.cpp

--- ThreadedBST.cpp
+++ ThreadedBST.cpp
@@ -1,5 +1,5 @@
-
 #include <iostream>
+#include <math.h> 
 #include "ThreadedBST.h"
 
 using namespace std;
@@ -31,67 +31,6 @@ ThreadedBST::~ThreadedBST() {
 	clear(root);
 }
 
-/**
-void ThreadedBST::Insert(const int& item) {
-	// if the tree is empty, create a new node and set it as root
-	if (root == nullptr) {
-		root = new BSTNode(item);
-		return;
-	}
-	// start with the root node
-	BSTNode* curr = root;
-	// pointer to store the parent of the current node
-	BSTNode* parent = nullptr;
-
-	// traverse the tree and find the parent node of the given item
-	while (curr != nullptr) {
-		// update the parent to the current node
-		parent = curr;
-
-		// if the given item is less than the current node, go to the
-		// left subtree; otherwise, go to the right subtree.
-		if (item < curr->item) {
-			if (curr->isThreadedLeft == true) {
-				break;
-			}
-			else {
-				curr = curr->leftChildPtr;
-			}
-		}
-		else {
-			if (curr->isThreadedRight == true) {
-				break;
-			}
-			else {
-				curr = curr->rightChildPtr;
-			}
-		}
-	} //end while loop
-
-	// construct a node and assign it to the appropriate parent pointer
-	BSTNode* newNode = new BSTNode(item);
-	newNode->isThreadedRight = newNode->isThreadedLeft = true;
-
-	if (item < parent->item) {
-		//set newNode's left ptr to its parent's old left ptr
-		newNode->leftChildPtr = parent->leftChildPtr;
-		//newNode's right ptr points back at parent
-		newNode->rightChildPtr = parent;
-		//parent points to newNode
-		parent->leftChildPtr = newNode;
-		//set parent's isThreadedLeft(bool) to false as it now has 
-		// a lwft node
-		parent->isThreadedLeft = false;
-	}
-	else {
-		newNode->rightChildPtr = parent->rightChildPtr;
-		newNode->leftChildPtr = parent;
-		parent->rightChildPtr = newNode;
-		parent->isThreadedRight = false;
-	}
-}
-
-*/
 
 void ThreadedBST::insertMultipleNodes(const int& min, const int& max) {
 	int middle = (min + max) / 2;
@@ -189,14 +128,6 @@ BSTNode* ThreadedBST::getFarthestRight(BSTNode* node) const {
 int ThreadedBST::getDepth() const {
 	return log2(count + 1);
 }
-
-/**
-void ThreadedBST::Inorder() {
-	Inorder(root);
-}
-*/
-
-
 
 void ThreadedBST::clear(BSTNode* node) {
 	//Case 1: Node is not empty
