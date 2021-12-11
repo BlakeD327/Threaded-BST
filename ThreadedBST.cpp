34045aee0ed75fbfe27a4c47f3a4866ef89190de
BlakeD327 <47874373+BlakeD327@users.noreply.github.com>
2021-12-10 20:56:27 -0800

    Update ThreadedBST.cpp

--- ThreadedBST.cpp
+++ ThreadedBST.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <math.h>
 #include "ThreadedBST.h"
@@ -18,6 +17,7 @@ ThreadedBST::ThreadedBST() {
 	count = 0;
 }
 
+/*Set number of nodes
 //Set number of nodes
 ThreadedBST::ThreadedBST(const int& numOfNodes) {
 	root = nullptr;
@@ -26,32 +26,31 @@ ThreadedBST::ThreadedBST(const int& numOfNodes) {
 		insertMultipleNodes(1, numOfNodes);
 		setThread(root);
 	}
-}
+}*/
 
 ThreadedBST::~ThreadedBST() {
 	clear(root);
 }
 
-/**
-void ThreadedBST::Insert(const int& item) {
+
+ //MINE
+BSTNode* ThreadedBST::insert(BSTNode* node, const int& newItem) {
 	// if the tree is empty, create a new node and set it as root
 	if (root == nullptr) {
-		root = new BSTNode(item);
-		return;
+		root = new BSTNode(newItem);
+		return root;
 	}
 	// start with the root node
 	BSTNode* curr = root;
 	// pointer to store the parent of the current node
 	BSTNode* parent = nullptr;
-
 	// traverse the tree and find the parent node of the given item
 	while (curr != nullptr) {
 		// update the parent to the current node
 		parent = curr;
-
 		// if the given item is less than the current node, go to the
 		// left subtree; otherwise, go to the right subtree.
-		if (item < curr->item) {
+		if (newItem < curr->item) {
 			if (curr->isThreadedLeft == true) {
 				break;
 			}
@@ -68,12 +67,10 @@ void ThreadedBST::Insert(const int& item) {
 			}
 		}
 	} //end while loop
-
 	// construct a node and assign it to the appropriate parent pointer
-	BSTNode* newNode = new BSTNode(item);
+	BSTNode* newNode = new BSTNode(newItem);
 	newNode->isThreadedRight = newNode->isThreadedLeft = true;
-
-	if (item < parent->item) {
+	if (newItem < parent->item) {
 		//set newNode's left ptr to its parent's old left ptr
 		newNode->leftChildPtr = parent->leftChildPtr;
 		//newNode's right ptr points back at parent
@@ -90,10 +87,11 @@ void ThreadedBST::Insert(const int& item) {
 		parent->rightChildPtr = newNode;
 		parent->isThreadedRight = false;
 	}
+	return node;
 }
 
-*/
 
+/**
 void ThreadedBST::insertMultipleNodes(const int& min, const int& max) {
 	int middle = (min + max) / 2;
 	//recursive case
@@ -110,11 +108,12 @@ void ThreadedBST::insertMultipleNodes(const int& min, const int& max) {
 		}
 	}
 }
+*/
 
 void ThreadedBST::insert(const int& newItem) {
 	insert(root, newItem);
 }
-
+/** new inserts
 BSTNode* ThreadedBST::insert(BSTNode* node, const int& newItem) {
 	//Case 1: If the tree is empty, return a single new
 	if (node == nullptr) {
@@ -126,8 +125,6 @@ BSTNode* ThreadedBST::insert(BSTNode* node, const int& newItem) {
 		return new BSTNode(newItem);
 	}
 	BSTNode* tempNode = nullptr;
-
-	//Case 2: Go recursively through the tree
 	if (newItem < node->item) {
 		tempNode = insert(node->leftChildPtr, newItem);
 		node->leftChildPtr = tempNode;
@@ -136,10 +133,8 @@ BSTNode* ThreadedBST::insert(BSTNode* node, const int& newItem) {
 		tempNode = insert(node->rightChildPtr, newItem);
 		node->rightChildPtr = tempNode;
 	}
-
 	return node;
 }
-
 
 void ThreadedBST::setThread(BSTNode* node) {
 	//checks if node exists
@@ -164,7 +159,6 @@ void ThreadedBST::setThread(BSTNode* node) {
 	}
 }
 
-
 BSTNode* ThreadedBST::getFarthestLeft(BSTNode* node) const {
 	//checks if Node is null
 	if (node == nullptr)
@@ -176,7 +170,6 @@ BSTNode* ThreadedBST::getFarthestLeft(BSTNode* node) const {
 
 	return node;
 }
-
 BSTNode* ThreadedBST::getFarthestRight(BSTNode* node) const {
 	//checks if Node is null
 	if (node == nullptr)
@@ -189,18 +182,11 @@ BSTNode* ThreadedBST::getFarthestRight(BSTNode* node) const {
 	return node;
 
 }
+*/
 
 int ThreadedBST::getDepth() const {
 	return log2(count + 1);
 }
-
-/**
-void ThreadedBST::Inorder() {
-	Inorder(root);
-}
-*/
-
-
 
 void ThreadedBST::clear(BSTNode* node) {
 	//Case 1: Node is not empty
@@ -229,3 +215,91 @@ void ThreadedBST::clear(BSTNode* node) {
 		}
 	}
 }
+
+
+bool ThreadedBST::hasLeftChild(BSTNode* targetNode) {
+	BSTNode* temp = targetNode;
+	return(!temp->isThreadedLeft && temp->leftChildPtr != nullptr);
+}
+
+bool ThreadedBST::hasRightChild(BSTNode* targetNode) {
+	BSTNode* temp = targetNode;
+	return(!temp->isThreadedRight && temp->rightChildPtr != nullptr);
+}
+
+/**
+ * Calls the real recursive in order traversal and hands it the tree's root
+ */
+void ThreadedBST::Inorder() {
+	recInorder(root);
+}
+
+/**
+ * Main recursive function for in order.
+ * The recursive function calls on print nodes to handle printing.
+ * @param root - the ThreadedBST tree for traversal
+ * @param printCount - count of elements */
+void ThreadedBST::recInorder(BSTNode* root) {
+    // as long as the root is not null, recurses left children, then right children
+    if(root != NULL)
+    {
+        if(hasLeftChild(root)) {
+            recPreOrder(root->leftChildPtr);
+        }
+        //printNodes(&root);
+		cout << root->item << endl;
+        if(hasRightChild(root)) {
+            recPreOrder(root->rightChildPtr);
+        }        
+    }
+}
+
+/**
+ * Calls the real recursive pre order traversal and hands it the tree's root
+ */
+void ThreadedBST::PreOrder(){
+    recPreOrder(root);
+}
+
+/**
+ * Main recursive function for preorder.
+ * The recursive function calls on print nodes to handle printing.
+ * @param root - the ThreadedBST tree for traversal
+ * @param printCount - count of elements
+ */
+void ThreadedBST::recPreOrder(BSTNode* root) {
+    // as long as the root is not null, recurses left children, then right children
+    if(root != NULL) {
+        //printNodes(&root);  
+		cout << root->item << endl;      
+        if(hasLeftChild(root)) {
+            recPreOrder(root->leftChildPtr);
+        }        
+        if(hasRightChild(root)) {
+            recPreOrder(root->rightChildPtr);
+        }
+    }
+}
+
+/**
+void ThreadedBST::Inorder() {
+	//recInorder(root);
+	if (root == nullptr) {
+		return;
+	}
+	BSTNode* curr = root;
+	while(curr->leftChildPtr != nullptr) {
+		curr = curr->leftChildPtr;
+	}
+	while (curr != nullptr) {
+		cout << curr->item << endl;
+		curr = curr->rightChildPtr;
+	}
+}
+*/
+/**
+void ThreadedBST::recInorder(BSTNode* root) {
+	if (root == nullptr) {
+		return;
+	}	
+}*/
