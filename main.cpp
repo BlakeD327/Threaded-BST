9b4bb182df829d4a976e4e9a2c5ad64e550043c7
Jaylon Nelson-Sellers <jcns@uw.edu>
2021-12-11 17:22:37 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,34 @@
+
 #include <iostream>
 #include "ThreadedBST.h"
 
 using namespace std;
 
 // Driver code
-int main()
+int main(int argc, char** argv)
 {
-    ThreadedBST newTree, *root = NULL;
-    root = newTree.Insert(root, 5);
-    newTree.Insert(root, 3);
-    newTree.Insert(root, 2);
-    newTree.Insert(root, 4);
-    newTree.Insert(root, 7);
-    newTree.Insert(root, 6);
-    newTree.Insert(root, 8);
- 
-    newTree.Inorder(root);
+    ThreadedBST testArgs(atoi(argv[0]));
+    testArgs.inorder();
+
+
+    //Demo Test
+    ThreadedBST test1(35);
+    test1.inorder();
+    cout << endl;
+
+    ThreadedBST test2(test1);
+    test2.inorder();
+    cout << endl;
+
+    ThreadedBST b;
+    b.insert(5);
+    b.insert(3);
+    b.insert(2);
+    b.insert(4);
+    b.insert(7);
+    b.insert(6);
+    b.insert(8);
+
+    b.inorder();
     return 0;
 }
